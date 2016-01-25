266439d041e1ce8eed1bb38686f01e6975e9e3aa
sheebypanda <sheebypanda@gmail.com>
2016-01-25 21:59:51 +0100

    add pic in index

--- app/models/drummer.rb
+++ app/models/drummer.rb
@@ -1,4 +1,5 @@
 class Drummer < ActiveRecord::Base
+  validates :name, :picture, presence: true 
   has_many :drums
-  mount_uploader :picture, :PictureUploader
+  mount_uploader :picture, PictureUploader
 end
