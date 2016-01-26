ae4555781e89313b0d9138691804cd6aaf9abc59
sheebypanda <sheebypanda@gmail.com>
2016-01-26 11:32:24 +0100

    Add custom css and root link

--- app/models/drummer.rb
+++ app/models/drummer.rb
@@ -1,5 +1,5 @@
 class Drummer < ActiveRecord::Base
-  validates :name, :picture, presence: true 
+  validates :name, presence: true, uniqueness: true
   has_many :drums
   mount_uploader :picture, PictureUploader
 end
