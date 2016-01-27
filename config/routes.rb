4bc097965c7ff34b8ce38d13050ed1618b26634e
sheebypanda <sheebypanda@gmail.com>
2016-01-27 21:44:19 +0100

    open routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :drummers do
-    resources :drums, only: [:new, :create, :index]
+    resources :drums, only: [:new, :create, :index, :destroy, :show]
   end
+  resources :drums
 end
