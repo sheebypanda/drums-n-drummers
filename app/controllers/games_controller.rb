907822a711b819871637ca603bba52516420e447
sheebypanda <sheebypanda@gmail.com>
2016-02-25 20:47:12 +0100

    start singleing drums

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,35 +1,30 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [ :play, :check ]
+  def  @@lvl = 1
+  end
 
   def welcome
   end
   def about
   end
   def play
-    @lvl = 1
-    @jocker = 1
   end
   def check
-    lvl = params[:lvl].to_i
     jocker = params[:jocker].to_i
     drummer_id = params[:id].to_i
     drum_id = params[:drum]
     drum = Drum.find(drum_id)
-    if jocker > 0
-      @jocker = jocker
-      render 'play'
-    elsif drummer_id == drum.drummer_id
-      lvl += 1
-      @lvl = lvl.to_s
+    if drummer_id == drum.drummer_id
+      @@lvl += 1
+      @lvl = @@lvl.to_s
       flash.now[:notice] = "Well done !"
       render 'play'
     elsif drummer_id != drum.drummer_id
-      @lvl = lvl.to_s
+      @lvl = @@lvl.to_s
       @drum = drum
       flash.now[:alert] = "Miss..."
       render 'loose'
     else
-      @lvl = "0"
       render 'play'
     end
   end
@@ -38,10 +33,13 @@ class GamesController < ApplicationController
 
   private
 
-    def set_game
-      @drum = Drum.limit(1).order("RANDOM()")
-      @drummers = Drummer.where.not(id: @drum.first.drummer_id).limit(2).order("RANDOM()")
-      @drummers << @drum.first.drummer
-      @drummers = @drummers.shuffle
-    end
+  def set_game
+    @drums = Drum.all
+    @drum = @drums[@@lvl]
+    # @drum = Drum.limit(1).order("RANDOM()")
+    @drummers = Drummer.where.not(id: @drum.drummer_id).limit(2).order("RANDOM()")
+    @drummers << @drum.drummer
+    @drummers = @drummers.shuffle
+  end
+
 end
