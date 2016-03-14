23e677072eeb712a8b47ea431cd0163153ee6fe8
sheebypanda <sheebypanda@gmail.com>
2016-03-14 19:03:14 +0100

    try cooking

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,13 @@
 class GamesController < ApplicationController
-  attr_accessor :lvl, :drums
+  attr_accessor :lvl
 
   def welcome
   end
   def about
   end
+  def congrat
+  end
+
   def play
     lvlup(lvl)
     if lvl == 1
@@ -17,13 +20,13 @@ class GamesController < ApplicationController
     @drummers = @drummers.shuffle
     render 'play'
   end
+
   def loose
       @drum = Drum.find(params[:drum])
       flash.now[:alert] = "Miss..."
       render 'loose'
   end
-  def congrat
-  end
+
   def check
     @lvl = params[:lvl].to_i
     drummer_id = params[:id].to_i
@@ -40,23 +43,29 @@ class GamesController < ApplicationController
 
   private
 
-  def lvlmax
-    @lvlmax = drums.count
+  def lvlup(value)
+    @lvl = 1 + value.to_i
   end
+  def lvlmax
+    @lvlmax = Drum.all.count
+  end
+
   def set_drums
-    cookies[:drums] = Drum.all.shuffle
+    @drumsids = []
+    Drum.all.shuffle.each do |d|
+      @drumsids << d.id
+    end
+    cookies[:drums] = @drumsids
   end
   def get_drums
-    @drums = cookies[:drums]
-  end
-  def drum
-    @drum = drums[lvl]
+    @drums = cookies[:drums].split('&')
   end
   def drums
     @drums = cookies[:drums]
   end
-  def lvlup(value)
-    @lvl = 1 + value.to_i
+  def drum
+    @drum = Drum.find(drums[lvl])
   end
+
 
 end
